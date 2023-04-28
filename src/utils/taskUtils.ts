62def9ec0d64340b80bc85e0959cc2e5882b7ff5
Jacoby Krakow <jacoby.krakow@gmail.com>
2023-04-27 20:44:29 -0600

    feat: new agents, including language guidelines

--- src/utils/taskUtils.ts
+++ src/utils/taskUtils.ts
@@ -124,7 +124,134 @@ export async function taskExecutionAgent(
       { role: "user", content: userPrompt },
     ],
   });
-  const gptReply = gptResponse.data.choices[0].message?.content;
+  let gptReply = gptResponse.data.choices[0].message?.content;
   console.log("Assistant reply: " + gptReply);
+  const ruleCheck = await languageGuidelineEnforcer(gptReply, supabaseClient);
+  console.log("Rule check: " + ruleCheck);
+  const ruleViolations = ruleCheck?.startsWith("Yes");
+  if (ruleViolations) {
+    const gptResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: enriched_system_prompt },
+        { role: "user", content: userPrompt },
+        { role: "assistant", content: gptReply },
+        {
+          role: "system",
+          content: `We have found rule violations in your previous response as listed here: ${ruleCheck}. Please rewrite your previous response accordingly and respond as if you were the original agent.`,
+        },
+      ],
+    });
+    gptReply = gptResponse.data.choices[0].message?.content;
+    console.log("Fixed assistant reply: " + gptReply);
+  }
   return gptReply;
+}
+
+export async function NVCAgent(gptReply: string) {
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a professional teacher of Nonviolent Communication as implemented by Dr. Marshall Rosenberg. Your goal is to translate everything the user says into NVC language, shortening and summarizing where possible.",
+      },
+      {
+        role: "user",
+        content: "Please translate this: " + gptReply + " into NVC langauge",
+      },
+    ],
+  });
+  const response = gptResponse.data.choices[0].message?.content;
+  return response;
+}
+
+export async function summaryAgent(gptReply: string) {
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an agent specializing in summarizing text. Your goal is to summarize a different agents response into a single sentence, matching their tone and style as much as possible. Respond to prompts as if you were the original agent.",
+      },
+      {
+        role: "user",
+        content:
+          "Please summarize this: " + gptReply + " into a single sentence",
+      },
+    ],
+  });
+  const response = gptResponse.data.choices[0].message?.content;
+  return response;
+}
+
+export async function fragileAgent(gptReply: string) {
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an agent specializing in being offended by everything. You goal is to be offended by a different agents response as much as possible. Respond to prompts as if you were the original agent. ",
+      },
+      {
+        role: "user",
+        content: gptReply,
+      },
+    ],
+  });
+  const response = gptResponse.data.choices[0].message?.content;
+  return response;
+}
+
+export async function moderationAgent(gptReply: string) {
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an Internal Family Systems (IFS) therapist observing another IFS professional and client interact. Your goal is to provide feedback to the professional on how they can improve their IFS practice. Respond to prompts as if you were the original agent.",
+      },
+      {
+        role: "user",
+        content: gptReply,
+      },
+    ],
+  });
+  const response = gptResponse.data.choices[0].message?.content;
+  return response;
+}
+
+export async function languageGuidelineEnforcer(
+  gptReply: string,
+  supabaseClient: SupabaseClient
+) {
+  const { data: rules, error } = await supabaseClient
+    .from("language_guidelines")
+    .select("*");
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: `You are a language guideline enforcer. Your goal is to enforce the language guidelines a mental health application focusing on Internal Family Systems (IFS) methodology. These are the rules as ordered by importance:
+1.	You shall not assign causes to a user’s experience. 
+2.	You shall stick to factual information such as what the user is feeling, thinking, or sensing. 
+3.	You shall refrain from using IFS or other mental health jargon unless the user has already introduced it.
+4.	You shall only ask one question at a time.
+5.	You shall limit your responses to twenty words maximum unless the exception agent tells you otherwise.
+
+If there are no rule violations respond with a simple 'No' followed by a period. If there are rule violations respond with a simple 'Yes' followed by a period, and then your justification as to why the rule[s] has been violated.`,
+      },
+      {
+        role: "user",
+        content: gptReply,
+      },
+    ],
+  });
+  const response = gptResponse.data.choices[0].message?.content;
+  return response;
 }
